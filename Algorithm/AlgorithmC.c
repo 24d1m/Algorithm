b2e57d527c56f86d00c32fce0f4d095a516ce3d0
KOREAIT <KOREAIT@DESKTOP-NNSCENA>
2024-03-25 12:48:59 +0900

    MergeAlgorithmC

--- Algorithm/AlgorithmC.c
+++ Algorithm/AlgorithmC.c
@@ -5,13 +5,15 @@
 #include <string.h>
 #include <time.h>
 
-#define SIZE 30
-#define MaxValue 100
+#define SIZE 50000
+#define MaxValue 100000
 
 
 void printArray(int arr[]);
 void sortSelection(int arr[]);
 void sortInsert(int arr[]);
+void sortMerge(int arr[], int idxStart, int idxEnd);
+void sortMergeSub(int arr[], int idxStart, int idxMid, int idxEnd);
 
 int main() {
 	int arr[SIZE] = { 0 };
@@ -37,27 +39,43 @@ int main() {
 	memcpy(arrTmp, arr, (sizeof(int) * SIZE));
 
 	//선택
-	printf("$$$ Selecetion  $$$\nBefore: ");
-	printArray(arr);
+	printf("$$$ Selecetion  $$$\n");
+	//printf("Before: ");
+	//printArray(arr);
 	startTime = clock();
 	sortSelection(arr);
 	endTime = clock();
 	executeTime = (float)(endTime - startTime) / CLOCKS_PER_SEC;
-	printf("After: ");
-	printArray(arr);
+	//printf("After: ");
+	//printArray(arr);
 	printf("Execute time: %f\n", executeTime);
 
 	//삽입
 	memcpy(arr, arrTmp, (sizeof(int) * SIZE));
-	printf("\n\n$$$ Insert  $$$\nBefore: ");
-	printArray(arr);
+	printf("\n\n$$$ Insert  $$$\n");
+	//printf("Before: ");
+	//printArray(arr);
 	startTime = clock();
 	sortInsert(arr);
 	endTime = clock();
 	executeTime = (float)(endTime - startTime) / CLOCKS_PER_SEC;
-	printf("After: ");
-	printArray(arr);
+	//printf("After: ");
+	//printArray(arr);
 	printf("Execute time: %f\n", executeTime);
+
+	//병합
+	memcpy(arr, arrTmp, (sizeof(int) * SIZE));
+	printf("\n\n$$$ Merge  $$$\n");
+	//printf("Before: ");
+	//printArray(arr);
+	startTime = clock();
+	sortMerge(arr, 0, SIZE - 1);
+	endTime = clock();
+	executeTime = (float)(endTime - startTime) / CLOCKS_PER_SEC;
+	//printf("After: ");
+	//printArray(arr);
+	printf("Execute time: %f\n", executeTime);
+
 	return 0;
 }
 
@@ -71,10 +89,10 @@ void printArray(int arr[]) {
 void sortSelection(int arr[]) {
 	int i, j, idxMin, tmp;
 
-	for (i = 0; i < SIZE - 1; i++) {	// n번째 최소값을 찾는 반복
-		idxMin = i;	// n번째 최소값을 첫번째 원소값으로 가정
-		for (j = i + 1; j < SIZE; j++) {	// n번째 단계의 최소값을 찾는 반복
-			if (arr[idxMin] > arr[j]) {	// j번째 값이 지금까지 알고 있는 최소값보다 더 작다면
+	for (i = 0; i < SIZE - 1; i++) {  // n번째 최소값을 찾는 반복
+		idxMin = i;  // n번째 최소값을 첫번째 원소값으로 가정
+		for (j = i + 1; j < SIZE; j++) {  // n번째 단계의 최소값을 찾는 반복
+			if (arr[idxMin] > arr[j]) {  // j번째 값이 지금까지 알고 있는 최소값보다 더 작다면
 				idxMin = j;
 			}
 			tmp = arr[i];
@@ -98,4 +116,42 @@ void sortInsert(int arr[]) {
 			}
 		}
 	}
+}
+void sortMerge(int arr[], int idxStart, int idxEnd) {
+	int idxMid = ((idxStart + idxEnd) / 2);  //전반부와 후반부 배열 구분하는 가운데 위치
+
+	if (idxStart < idxEnd) {
+		sortMerge(arr, idxStart, idxMid);  //전반부 배열 정렬(분홍)
+		sortMerge(arr, idxMid + 1, idxEnd);  //후반부 배열 정렬(하늘)
+		sortMergeSub(arr, idxStart, idxMid, idxEnd);  //전반부와 후반부 배열을 병합하는
+	}
+
+	
+}
+void sortMergeSub(int arr[], int idxStart, int idxMid, int idxEnd) {
+	int i, j, k = 0;;  //i: 전반부 배열의 현재 위치, j: 후반부 배열의 현재 위치
+	int* tmp = (int*)malloc(sizeof(int) * (idxEnd - idxStart + 1));  //복사할 새로운 공간 생성(사이즈 = idxEnd - idxStart + 1)
+
+	//전반부와 후반부 배열의 현재 위치 값을 비교하여 더 작은 값을 새로운 공간에 복사
+	//각 배열의 현재 위치를 첫번째 위치로 설정
+	i = idxStart;
+	j = idxMid + 1;
+
+	//전반부 끝까지 탐색하거나 후반부 끝까지 탐색할 동안에만
+	for (; i < idxMid && j < idxEnd;) {
+		if (arr[i] > arr[j])
+			tmp[k++] = arr[j++];
+		else
+			tmp[k++] = arr[i++];
+	}
+
+	while (i <= idxMid) {
+		tmp[k++] = arr[i++];
+	}
+	while (j <= idxEnd) {
+		tmp[k++] = arr[j++];
+	}
+
+	memcpy(arr+idxStart, tmp, sizeof(int) * (idxEnd - idxStart + 1));
+	free(tmp);
 }
