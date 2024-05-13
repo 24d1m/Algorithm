55f9e028debb8af2812c6411983572d57aae98a4
KOREAIT <KOREAIT@DESKTOP-NNSCENA>
2024-05-13 12:42:30 +0900

    0513

--- Algorithm/bst.c
+++ Algorithm/bst.c
@@ -6,8 +6,8 @@
 #include <string.h>
 #include <time.h>
 
-#define SIZE 100
-#define MaxValue 1000
+#define SIZE 10
+#define MaxValue 15
 
 void printArray(int arr[]);
 
@@ -16,31 +16,45 @@ typedef struct bst {
 	struct bst* left, * right; //왼쪽 자식, 오른쪽 자식
 } BST;
 
+BST* insertBST(BST* node, int key);
+void printBST(BST* node);
+BST* searchBST(BST* node, int key);
+
 int main() {
-	BST* node;  //root노드를 가르키고 있는 메모리 주소
 	int arr[SIZE];
-	int key, idxKey;  //key = 탐색하고자 하는 데이터, idxKey = 탐색 값의 위치
+	int key = 13, idxKey;  //key = 탐색하고자 하는 데이터, idxKey = 탐색 값의 위치
 	int i, j;
 	unsigned int input;
 
-	//입력값 랜덤 & 중복
-	for (i = 0; i < SIZE; i++) {
+	for (i = 0; i < SIZE; i++) {  //입력값 랜덤 & 중복
 		rand_s(&input);
 		arr[i] = input % MaxValue;
 		for (j = 0; j < i; j++) {
-			if (arr[i] == arr[j]) {
+			if (arr[j] == arr[i]) {
 				i--;
 				break;
 			}
 		}
 	}
 
+	printArray(arr);  //정렬
+
+	BST* nodeRoot = NULL;  //root노드를 가르키고 있는 메모리 주소
+	BST* nodeSearch = NULL;  //탐색된 노드의 메모리 주소
 	for (i = 0; i < SIZE; i++) {
-		insertBST(node, arr[i]);  //root노드에 i번째 배열 값을 삽입
+		nodeRoot = insertBST(nodeRoot, arr[i]);  //root노드에 i번째 배열 값을 삽입
 	}
 
-	//정렬
-	printArray(arr);
+	printBST(nodeRoot);
+	nodeSearch = searchBST(nodeRoot, key);
+	if (nodeSearch != NULL) {
+		printf("탐색 성공!\t%d\n", nodeSearch->key);
+	}
+	else {
+		prtinf("탐색 실패!d\n");
+	}
+	deleteBST(nodeRoot, key);
+
 
 	return 0;
 }
@@ -53,19 +67,89 @@ void printArray(int arr[]) {
 	}
 	printf("\n");
 }
-insertBST(BST* node, int key) {
+BST* insertBST(BST* node, int key) {
 	if (node == NULL) {  //노드가 비었을 때 노드를 생성하고 키 값을 저장
 		BST* newNode = (BST*)malloc(sizeof(BST));  //사용할 메모리 할당
 
 		newNode->key = key;
 		newNode->left = NULL;
 		newNode->right = NULL;
+
+		return newNode;
 	}
 
 	if (key < node->key) {  //추가하려는 키가 현재 노드의 키 값보다 작다면 왼쪽 자식으로 추가
-		insertBST(node->left, key);
+		node->left = insertBST(node->left, key);
 	}
 	else {   //추가하려는 키가 현재 노드의 키 값보다 크다면 오른쪽 자식으로 추가
-		insertBST(node->right, key);
+		node->right = insertBST(node->right, key);
 	}
+	return node;
+}
+void printBST(BST* node) {
+	if (node != NULL) {
+		//왼쪽 자식의 노드를 방문해서 출력
+		printBST(node->left);
+		//현재 노드의 키 값을 출력
+		printBST("%d  ", node->key);
+		//오른쪽 자식의 노드를 방문해서 출력
+		printBST(node->right);
+	}
+}
+BST* searchBST(BST* node, int key) {
+	if (node == NULL) {
+		return node;
+	}
+	if (node->key == key) {
+		return node;
+	}
+	if (key < node->key) {
+		searchBST(node->left, key);
+	}
+	else {
+		searchBST(node->right, key);
+	}
+}
+BST* deleteBST(BST* node, int key) {
+	if (node == NULL) {
+		return NULL;
+	}
+	if (node->key == key) {  //삭제시작
+		if (node->left == NULL && node->right == NULL) {  //case 1 삭제하려고 하는 노드의 자식들이 둘다 없는 경우
+			free(node);
+			return NULL;
+		}
+		else if (node->left != NULL && node->right == NULL) {  //case 2 삭제하려고 하는 노드의 자식들이 하나만 없는 경우 - 왼쪽 자식만 있는 경우
+			BST* tmp = node->left;
+			free(node);
+			return tmp;
+		}
+		else if (node->left == NULL && node->right != NULL) {  //case 2-2 삭제하려고 하는 노드의 자식이 하나만 없는 경우 -오른쪽 자식만 있는 경우
+			BST* tmp = node->right;
+			free(node);
+			return tmp;
+		}
+		else {  //case 3 삭제하려고 하는 노드의 자식들이 둘다 있는 경우
+			//오른쪽 서브트리를 기준으로 구현
+			BST* nodeMin = getMinNode(node->right);  //오른쪽 서브트리 중에 가장 작은 값을 가지고 있는 노드를 찾음
+
+			node->key = nodeMin->key;  //삭제 노드의 키 값을 오른쪽 서브트리의 가장 작은 최소값으로 변경
+
+			node->right = deleteBST(node->right, nodeMin);
+		}
+	}
+	if (key < node->key) {
+		deleteBST(node->left, key);
+	}
+	else {
+		deleteBST(node->right, key);
+	}
+	return node;
+}
+BST* getMinNode(BST* node) {
+	BST* nodeMin = node;
+	while (nodeMin->left != NULL) {
+		nodeMin = nodeMin->left;
+	}
+	return nodeMin;
 }
